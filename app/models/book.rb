95718eb62263bb93047e69eb64ecdbc6f4be402a
Michael Palhidai <mpalhidai@gmail.com>
2018-11-21 04:24:20 -0800

    removed old attribute

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,6 @@ class Book < ApplicationRecord
   validates :title, presence: true, length: { in: 2..50 }, uniqueness: { case_sensitive: false }
   validates :author, presence: true, length: { in: 2..50 }
   validates :category, presence: true
-  validates :complete, presence: true, inclusion: { in: 0..100 }
   validates :current_page, presence: true
   validates :current_chapter, presence: true
   validates :total_pages, presence: true
