b2348153cf4a5974afddf3dff12ac55d0b3bf87f
Michael Palhidai <mpalhidai@gmail.com>
2018-12-03 13:45:41 -0800

    removed diff tool nonsense

--- config/application.rb
+++ config/application.rb
@@ -26,19 +26,10 @@ module MybooksAPI
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-<<<<<<< HEAD
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options]
-      end
-    end
-=======
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
->>>>>>> dev
   end
 end
