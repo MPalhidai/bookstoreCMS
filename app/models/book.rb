f21830e40d2a7027d5b3a8c7c733a89443c75135
Michael Palhidai <mpalhidai@gmail.com>
2018-12-13 19:53:29 -0800

    book has many comments association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { in: 2..50 }, uniqueness: { case_sensitive: false }
   validates :author, presence: true, length: { in: 2..50 }
   validates :category, presence: true
