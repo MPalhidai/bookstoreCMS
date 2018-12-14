71a61f64a5f10933bd16dd77fb2fb2b6aa9fc9eb
Michael Palhidai <mpalhidai@gmail.com>
2018-12-13 19:10:03 -0800

    changed model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :book
 
   validates :user, presence: true, length: { in: 2..64 }
-  validates :rating, presence: true, range: { in: 0..100 }
+  validates :rating, presence: true, inclusion: { in: 0..100 }
   validates :content, presence: true, length: { in: 2..256 }
 end
